b97caa61dc69f82fa578ee7d80f41922564b90fd
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2025-07-29 18:27:15 -0300

    feat: Configuração de rotas do app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,34 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Layout from "../layout"
 import Auth from "../pages/Auth"
+import Page from "@/pages/dashboard/page";
+import Profile from "@/pages/Profile";
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
+                {/* Rota pública - Página de autenticação */}
                 <Route path="/auth" element={<Auth />} />
+
+                {/* Rota principal - Dashboard com sidebar */}
+                <Route
+                    path="/"
+                    element={
+                        <Layout>
+                            <Page />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <Layout>
+                            <Profile />
+                        </Layout>
+                    }
+                />
             </Routes>
         </Router>
     );
